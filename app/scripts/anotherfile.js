807a055c916c92851a91d1d80b295b50c462bf53
Nick-Damico <nickalan82@icloud.com>
2018-04-21 23:38:32 -0400

    rename js/, css/ to scripts/ and styles/. add jasmine-console-reporter

--- app/scripts/anotherfile.js
+++
@@ -1,3 +0,0 @@
-function divideNums(num1, num2) {
-  return num1 / num2;
-}
--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -1,16 +1,6 @@
-const test_js = require('../app/js/test');
+const test_js = require('../app/scripts/app');
 
 describe('Tests', function() {
-  it('adds two numbers', function(){
-    const number = test_js.add(2,2);
-    expect(number).toEqual(4);
-  });
-
-  it('subtracts two numbers', function() {
-    const number = test_js.subtract(5, 10);
-    expect(number).toEqual(5);
-  });
-
   it('divides two numbers', function() {
     const number = test_js.divide(10,5);
     expect(number).toEqual(2);
