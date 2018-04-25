35d83472111d7ef7b99fc09c92124b18d9173877
Nick-Damico <nickalan82@icloud.com>
2018-04-25 11:08:40 -0400

    add Handlebars.js to project v.4.0.11, add script tags for loading Handlebars, build channel template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,12 @@
-function divide(num1, num2) {
-  return num1 / num2;
-}
+// Get Elements from DOM
+const channelsContainer = document.getElementById('channelsContainer');
 
-module.exports = {
-  divide: divide
-}
+// Get Handlebars Templates from index.html
+const source = document.getElementById('channel-template');
+// Compile source to template
+let template = Handlebars.compile(source.innerHTML);
+// Build template with data
+const content = {id: 9, title: 'Happy Stream', status: 'Live'};
+const html = template(content);
+// Append to the DOM
+channelsContainer.innerHTML = html
