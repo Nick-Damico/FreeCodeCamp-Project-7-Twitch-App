387af1ea8fd75ef42c6b320048cca77c54808681
Nick-Damico <nickalan82@icloud.com>
2018-04-26 12:15:44 -0400

    selecting new template and compiling, define Classes TwitchApp and Streamer with methods for getting data from Twitch API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,71 @@
-// Get Elements from DOM
+// Get Element Referneces from DOM
 const channelsContainer = document.getElementById('channelsContainer');
+const featuredSection = document.getElementById('featuredSection');
 
-// Get Handlebars Templates from index.html
-const source = document.getElementById('channel-template');
-// Compile source to template
-let template = Handlebars.compile(source.innerHTML);
-// Build template with data
-const content = {id: 9, title: 'Happy Stream', status: 'Live'};
-const html = template(content);
-// Append to the DOM
+// Compile tempates
+const featureSource = document.getElementById('featured-template');
+let featureTemplate = Handlebars.compile(featureSource.innerHTML);
+const channelSource = document.getElementById('channel-template');
+let channelTemplate = Handlebars.compile(channelSource.innerHTML);
+
+// Get HTML from evaluating Handlebars template
+const content = {
+  id: 9,
+  title: 'Happy Stream',
+  status: 'Live'
+};
+const html = channelTemplate(content);
+
+// Append Compiled HTML into the DOM
 channelsContainer.innerHTML = html
+
+// Array of streamers tracked in this program
+const twitchStreamers = [
+  "freecodecamp",
+  "therunningmanz",
+  "kotton",
+  "giantwaffle",
+  "dakotaz",
+  "geekandsundry",
+  "anneMunition",
+  "yogscast",
+  "smoke"
+]
+
+// Streamer Class, building class instances of 'Streamer'
+class Streamer {
+  constructor(id, name) {
+    this.name   = name;
+    this.id     = id;
+  }
+}
+
+// Twitch Class, functions as Class object 'Static'
+class TwitchApp {
+  static url() {
+    return "https://wind-bow.gomix.me/twitch-api/streams/";
+  }
+
+  // Takes instance of Streamer Class and fetches stream info,
+    // returned data builds out instance further with new properties.
+  static getStreamerInfo(streamer) {
+    $.getJSON(this.url() + streamer.name + '?callback=?', ( data ) => {
+      if (data.stream == null) {
+        console.log('not live');
+      } else {
+        let streamData = data.stream;
+        streamer.game = streamData.game;
+        streamer.title = streamData.channel.status;
+        streamer.logo = streamData.channel.logo;
+        streamer.banner = streamData.channel.profile_banner;
+        streamer.followers = streamData.channel.followers;
+        streamer.views = streamData.channel.views;
+        streamer.url = streamData.channel.url;
+
+        console.log(streamer);
+      }
+    });
+  }
+}
+runningManZ = new Streamer(1, twitchStreamers[1]);
+TwitchApp.getStreamerInfo(runningManZ);
