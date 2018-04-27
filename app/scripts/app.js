8d29e7ba9ed282288cb05ee3bd9f79f29f699c3e
Nick-Damico <nickalan82@icloud.com>
2018-04-27 01:01:07 -0400

    add default images for channel__cards

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,13 +7,19 @@ const featureSource = document.getElementById('featured-template');
 const featureTemplate = Handlebars.compile(featureSource.innerHTML);
 const channelSource = document.getElementById('channel-template');
 const channelTemplate = Handlebars.compile(channelSource.innerHTML);
-
+let fetchStatus = false;
 // Streamer Class, building class instances of 'Streamer'
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
 class Streamer {
-  constructor(id, name) {
-    this.status = false;
-    this.id     = id;
-    this.name   = name;
+  constructor(id, streamer) {
+    this.id = id;
+    this.status = 'Offline';
+    this.name = streamer.name;
+    this.image = streamer.image;
 
     // Adding instance to the Streamer Class all collection.
     Streamer.all(this);
@@ -34,8 +40,9 @@ class Streamer {
   static buildStreamers(array) {
     let count = 1;
     // iterates over array of strings instantiate them as Objects
-    for(let streamer of array) {
-      new Streamer( count++, streamer);
+    for (let streamer of array) {
+
+      new Streamer(count++, streamer);
     }
   };
 }
@@ -47,25 +54,26 @@ class TwitchApp {
   }
 
   // Takes instance of Streamer Class and fetches stream info,
-    // returned data builds out instance further with new properties.
+  // returned data builds out instance further with new properties.
   static getStreamerInfo(streamer) {
-    $.getJSON(this.url() + streamer.name + '?callback=?', ( data ) => {
-      if (!data.stream) {
-        console.log('offline');
-      } else {
+    $.getJSON(this.url() + streamer.name + '?callback=?', (data) => {
+      // debugger;
+      if (data.stream) {
         // store response JSON Data
         const stream = data.stream;
-
-        streamer.status     = true;
-        streamer.followers  = stream.channel.followers;
-        streamer.banner     = stream.channel.profile_banner;
-        streamer.title      = stream.channel.status;
-        streamer.views      = stream.channel.views;
-        streamer.game       = stream.game;
-        streamer.logo       = stream.channel.logo;
-        streamer.url        = stream.channel.url;
-
-        console.log(streamer);
+        streamer.status = 'Live';
+        streamer.followers = stream.channel.followers;
+        streamer.banner = stream.channel.profile_banner;
+        streamer.preview = stream.preview.large;
+        streamer.title = stream.channel.status;
+        streamer.views = stream.channel.views;
+        streamer.game = stream.game;
+        streamer.logo = stream.channel.logo;
+        streamer.url = stream.channel.url;
+      }
+      if (streamer.id === 9) {
+        this.appendChannelCards();
+        this.getFeatured();
       }
     });
   };
@@ -74,23 +82,55 @@ class TwitchApp {
     let streamers = Streamer.all();
 
     for (let streamer of streamers) {
-      this.getStreamerInfo( streamer );
+      this.getStreamerInfo(streamer);
     }
-    console.log( Streamer.all() );
+
   };
+
+  static appendChannelCards() {
+    const channelCardsHTML = channelTemplate({streamers: Streamer.all()});
+    channelsContainer.innerHTML = channelCardsHTML;
+  }
+
+  static getFeatured() {
+    let liveStreams = Streamer.all().filter((streamer) => streamer.status === 'Live');
+    let randNum = randomNumber(liveStreams.length);
+    let featuredStreamer = liveStreams[randNum];
+    const featureHTML = featureTemplate(featuredStreamer);
+    featuredSection.innerHTML = featureHTML;
+  }
 }
 
 // Array of streamers tracked in this program
 const twitchStreamers = [
-  "freecodecamp",
-  "therunningmanz",
-  "kotton",
-  "giantwaffle",
-  "dakotaz",
-  "geekandsundry",
-  "anneMunition",
-  "yogscast",
-  "ninja"
+  {
+    name: "freecodecamp",
+    image: 'images/fcc__card.png'
+  }, {
+    name: "therunningmanz",
+    image: 'images/runningz__card.png'
+  }, {
+    name: "kotton",
+    image: 'images/kotton__card.png'
+  }, {
+    name: "giantwaffle",
+    image: 'images/waffle__card.png'
+  }, {
+    name: "dakotaz",
+    image: 'images/dakotaz__card.png'
+  }, {
+    name: "geekandsundry",
+    image: 'images/gs__card.png'
+  }, {
+    name: "anneMunition",
+    image: 'images/annemunition__card.png'
+  }, {
+    name: "ninja",
+    image: 'images/ninja__card.png'
+  }, {
+    name: "yogscast",
+    image: 'images/yogs__card.png'
+  }
 ];
 
 Streamer.buildStreamers(twitchStreamers);
@@ -99,14 +139,13 @@ TwitchApp.buildChannelCards();
 // runningManZ = new Streamer(1, twitchStreamers[1]);
 // TwitchApp.getStreamerInfo(runningManZ);
 
-
-// Get HTML from evaluating Handlebars template
-const content = {
-  id: 9,
-  title: 'Happy Stream',
-  status: 'Live'
-};
-const html = channelTemplate(content);
-
-// Append Compiled HTML into the DOM
-channelsContainer.innerHTML = html
+//  Get HTML from evaluating Handlebars template
+// const content = {
+//   id: 9,
+//   title: 'Happy Stream',
+//   status: 'Live'
+// };
+// const html = channelTemplate(content);
+//
+//  Append Compiled HTML into the DOM
+// channelsContainer.innerHTML = html
