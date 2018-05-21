2a2fe03d61bfbdfb0225f3940a6f0e7542b2349c
Nick-Damico <nickalan82@icloud.com>
2018-05-20 22:39:19 -0400

    move updating live counter container to seperate method called updateLiveCounter on the App class, add comments for methods, passing updateLiveContainer as callback method to the getTwitchAPI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,12 @@
 class App {
-
   init() {
     // Build Streamer Objects
     Streamer.init();
     // Create channel__card HTML using Template and append to DOM.
     Twitch.getTwitchAPI({
       appendChannelCards: this.appendChannelCards,
-      getFeatured: this.getFeatured
+      getFeatured: this.getFeatured,
+      updateLiveCounter: this.updateLiveCounter
     });
   }
 
@@ -16,21 +16,25 @@ class App {
     const channelSource = document.getElementById('channel-template');
     const channelTemplate = Handlebars.compile(channelSource.innerHTML);
     const channelCardsHTML = channelTemplate({streamers: Streamer.all()});
-
     channelsContainer.innerHTML = channelCardsHTML;
   }
 
   getFeatured() {
-    const liveCounter = document.getElementById('liveCount');
     const featuredSection = document.getElementById('featuredSection');
     const featureSource = document.getElementById('featured-template');
     const featureTemplate = Handlebars.compile(featureSource.innerHTML);
+    // Filter all Streamer channels with a status of 'Live'
     const liveStreams = Streamer.all().filter((streamer) => streamer.status === 'Live');
+    // Randomly select of the 'Live' Streamers to spotlight in Featured area
     const randNum = randomNumber(liveStreams.length);
     const featuredStreamer = liveStreams[randNum];
-
     const featureHTML = featureTemplate(featuredStreamer);
     featuredSection.innerHTML = featureHTML;
+  }
+
+  updateLiveCounter() {
+    const liveCounter = document.getElementById('liveCount');
+    const liveStreams = Streamer.all().filter((streamer) => streamer.status === 'Live');
     // Setting Couter to number of live streams
     liveCounter.textContent = liveStreams.length;
   }
